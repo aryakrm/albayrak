9820d4ef6b810ec2486c43e6fc7af6c2ddb72d0c
Arya Karimi <devaryakrm@gmail.com>
2023-06-06 18:15:09 +0300

    add intro video and logo in nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Nav from "../Nav/Nav.jsx";
+import "./Header.scss";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Nav />
+      <div className="intro">
+        <video loop muted autoPlay playsInline>
+          <source src="/assets/dokumVideo.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./Nav.scss";
 
 function Nav() {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <div className="nav-left">
+        <img src="/assets/albayrakLogo.png" alt="" />
+      </div>
+      <div className="nav-right"></div>
+    </nav>
+  );
 }
 
 export default Nav;
