d974a2053e0b4afe3781a21bc9347582c54e45c7
Arya Karimi <devaryakrm@gmail.com>
2023-06-07 12:17:08 +0300

    contact menu added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,8 @@ import Nav from "../Nav/Nav.jsx";
 import "./Header.scss";
 import { AiFillInstagram } from "react-icons/ai";
 import { AiFillYoutube } from "react-icons/ai";
+import { AiFillMail } from "react-icons/ai";
+import { BsWhatsapp } from "react-icons/bs";
 
 function Header() {
   return (
@@ -15,8 +17,21 @@ function Header() {
         <h1>Yeni Nesil Dökümcü</h1>
       </div>
       <div className="contact-menu">
-        <AiFillInstagram />
-        <AiFillYoutube />
+        <a
+          href="https://instagram.com/metalcastingturkey?igshid=ZDc4ODBmNjlmNQ=="
+          target="_blank"
+        >
+          <AiFillInstagram />
+        </a>
+        <a href="https://www.youtube.com/@ALBAYRAKDOKUM" target="_blank">
+          <AiFillYoutube />
+        </a>
+        <a href="mailto:info@albamtl.com" target="_blank">
+          <AiFillMail />
+        </a>
+        <a href="https://wa.me/905538846271" target="_blank">
+          <BsWhatsapp />
+        </a>
       </div>
     </header>
   );
