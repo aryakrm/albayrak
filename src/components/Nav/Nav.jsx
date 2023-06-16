5f2b8b1e11f801955da25b66c0adb55d7174a841
Arya Karimi <devaryakrm@gmail.com>
2023-06-16 18:33:10 +0300

    Positions edited and menu Hamburger button added

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,34 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 import "./Nav.scss";
 
 function Nav() {
+  const [active, setActive] = useState(false);
+
+  const menuTogglerHandler = () => {
+    setActive(!active);
+  };
+
   return (
     <nav>
       <div className="nav-left">
         <img src="/assets/albayrakLogo.png" alt="" />
       </div>
+      <div onClick={menuTogglerHandler} className="hamburger">
+        {active ? (
+          <AiOutlineClose className="_icon" />
+        ) : (
+          <GiHamburgerMenu className="_icon" />
+        )}
+      </div>
+      {active ? (
+        <div className="side-menu">
+          <img src="/assets/albayrakLogo.png" alt="" />
+        </div>
+      ) : null}
     </nav>
   );
 }
