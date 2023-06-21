d3f0babe4febcb3f0c968450e31d308836e2c3ae
Arya Karimi <devaryakrm@gmail.com>
2023-06-21 10:58:33 +0300

    About page added

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -9,8 +9,42 @@ function AboutUs() {
         <div className="text">
           <img src="./assets/al.png" alt="" />
           <h1>Hakkımızda</h1>
-          <h2></h2>
-          <p></p>
+          <p>
+            Albayrak Döküm ve Metal Sanayi, 2021 yılında kurulan bir döküm
+            firmasıdır. Genç ve dinamik yapımızla, döküm sektöründe hızla
+            büyümeyi hedefliyoruz. Uzman kadromuz ve deneyimli çalışanlarımızla,
+            en kaliteli döküm ürünlerini üretmek ve müşterilerimize üstün hizmet
+            sunmak için çaba gösteriyoruz. Teknolojik gelişmeleri yakından takip
+            ederek, modern döküm tekniklerini en iyi şekilde uyguluyor ve
+            müşteri beklentilerini karşılamayı amaçlıyoruz. Pik döküm, Sfero
+            döküm ve Çelik döküm hizmetlerimizle geniş bir ürün yelpazesine
+            sahibiz. Müşteri odaklı yaklaşımımız, güvenilirlik ve kalite
+            ilkelerimizle sektörde güçlü bir marka olmayı hedefliyoruz. Albayrak
+            Döküm ve Metal Sanayi olarak, işimize olan tutkumuz ve müşteri
+            memnuniyetine verdiğimiz önemle sektördeki başarı hikayemizi yazmaya
+            devam ediyoruz.
+          </p>
+          <h1>Misyon</h1>
+          <p>
+            Müşterilerimize yüksek kaliteli döküm ürünleri sunmak ve
+            ihtiyaçlarını en iyi şekilde karşılamaktir. Müşteri memnuniyetini ön
+            planda tutarak, sürekli olarak yenilikçi üretim yöntemlerini ve
+            teknolojilerini takip ederek, sektördeki lider konumumuzu sürdürmek
+            ve güçlendirmek için çalışıyoruz. Müşterilerimizin beklentilerini
+            aşmak için kalite, esneklik ve zamanında teslimat konularında
+            mükemmelliği hedefliyoruz.
+          </p>
+          <h1>Vizyon</h1>
+          <p>
+            Döküm sektöründe önde gelen bir firma olarak tanınmak ve
+            müşterilerimize mükemmellik sağlamaktir. Sürekli olarak yenilikçi
+            üretim tekniklerini benimseyerek, ürünlerimizin kalitesini artirıyor
+            ve müşterilerimize en iyi çözümleri sunmak için çaba gösteriyoruz.
+            Sürdürülebilirlik ilkelerini benimseyerek, çevreye duyarlı bir
+            yaklaşımla çalışıyor ve sektörde örnek bir marka olmayı
+            hedefliyoruz. Müşterilerimize değer veren, uzman kadromuzla birlikte
+            büyümeyi ve başarıyı sürdürmek istiyoruz.
+          </p>
         </div>
       </div>
     </div>
