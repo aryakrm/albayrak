713930cc9c5a0f3efb5a702d09349b9c39ccc691
Arya Karimi <devaryakrm@gmail.com>
2023-06-23 23:54:14 +0300

    about page updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -24,7 +24,7 @@ function About() {
           src="https://www.youtube.com/embed/MxUMyxZsUFs?modestbranding=0&controls=0&autoplay=1&loop=1"
           title="YouTube video player"
           frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowfullscreen
         ></iframe>
       </div>
