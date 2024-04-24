29e1e7e5736fc235e56563e29b16f6d581e0e560
Arya Karimi <devaryakrm@gmail.com>
2024-04-24 19:24:19 +0300

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -3,7 +3,7 @@ import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-const apiKey = "73e5dl8bMk72Yrn6PChp8A";
+const apiKey = "oe11OqVu51V5iIQnBM8YyQ";
 const loadPath = `https://api.i18nexus.com/project_resources/translations/{{lng}}/{{ns}}.json?api_key=${apiKey}`;
 
 i18next
@@ -16,9 +16,9 @@ i18next
     ns: ["default"],
     defaultNS: "default",
 
-    supportedLngs: ["tr", "en", "de", "ar", "ru", "it", "fr"],
-
+    supportedLngs: ["tr","en","de","ar","ru","it","fr"],
+    
     backend: {
-      loadPath: loadPath,
-    },
-  });
+      loadPath: loadPath
+    }
+  })
